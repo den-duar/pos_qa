ee63f811158e50a5ffabb85f8a2009f89b94a292
den-duar <dennnis.oliveiraduarte@gmail.com>
2026-08-22 09:10:55 -0300

    remove duplicate

--- pytest/codes - Pytest/modularizado/invalid/fatorial.py
+++
@@ -1,9 +0,0 @@
-def fatorial(n):
-    if n < 0:
-        raise ValueError("Fatorial não definido para números negativos")
-    if not isinstance(n, int):
-        raise TypeError("Fatorial só é definido para números inteiros")
-    if n == 0:
-        return 1
-    else:
-        return n * fatorial(n - 1)
