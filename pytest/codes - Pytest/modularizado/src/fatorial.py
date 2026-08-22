57f59e00a467e72caae511fb3a5dce554baa8038
den-duar <dennnis.oliveiraduarte@gmail.com>
2026-08-22 09:32:57 -0300

    added elif

--- pytest/codes - Pytest/modularizado/src/fatorial.py
+++ pytest/codes - Pytest/modularizado/src/fatorial.py
@@ -1,9 +1,9 @@
 def fatorial(n):
     if n < 0:
         raise ValueError("Fatorial não definido para números negativos")
-    if not isinstance(n, int):
+    elif not isinstance(n, int):
         raise TypeError("Fatorial só é definido para números inteiros")
-    if n == 0:
+    elif n == 0:
         return 1
     else:
         return n * fatorial(n - 1)
